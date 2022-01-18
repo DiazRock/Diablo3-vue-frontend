665e2b450554841ee3043b695d16d9d9f6dccf01
DiazRock <corolariodiaz@gmail.com>
2022-01-18 15:12:02 -0500

    Changes

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,11 @@ import { createStore } from 'vuex'
 
 // Import oauth
 import oauth from './modules/oauth'
+import loading from './modules/loading'
 
 export default createStore({
   modules: {
-    oauth
+    oauth,
+    loading
   }
 })
