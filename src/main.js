1659a6cabdeec09c085941dfabc85a0744db01e1
DiazRock <corolariodiaz@gmail.com>
2022-01-18 18:57:05 -0500

    Added plugins to main.js

--- src/main.js
+++ src/main.js
@@ -1,39 +1,18 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+// BootstrapVue
+import './plugins/bootstrapVue'
 
-// Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// Vue Font-Awesome
+import './plugins/fontAwesome'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 // CSS global
 import './assets/css/main.styl'
 
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
@@ -41,7 +20,6 @@ new Vue({
   methods: {
     // Nuestra función
     init () {
-      console.log('Hello 🌝')
       store.dispatch('oauth/getToken')
     }
   },
