91f5fb6777ab9d3284dcbbfd2fa2e37fcb5187d3
DiazRock <corolariodiaz@gmail.com>
2022-01-18 15:11:28 -0500

    Added loading component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <LoadLayout v-if="isLoading">
+      <BaseLoading/>
+    </LoadLayout>
+    <MainLayout v-else/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  #app
+    padding 60px 0
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    color #ffffff
+    background-color #15202b
 </style>
-<script language="JavaScript">
+<script>
+import { mapState } from 'vuex'
+import LoadLayout from './layout/LoadLayout'
+import MainLayout from './layout/MainLayout'
+import BaseLoading from '@/components/BaseLoading.vue'
+
 export default {
   created () {
+    console.log('In the created')
     this.$store.dispatch('oauth/getToken', null, { root: true })
+  },
+  name: 'App',
+  components: {
+    MainLayout,
+    LoadLayout,
+    BaseLoading
+  },
+  beforeMount () {
+    console.log('isLoading ', this.isLoading)
+  },
+  computed: {
+    // Uso: mapState(moduleName, { state })
+    ...mapState('loading', {
+      isLoading: 'isLoading'
+    })
   }
 }
 </script>
