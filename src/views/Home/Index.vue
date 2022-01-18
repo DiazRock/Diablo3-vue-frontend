f3a776d7061a08674869cd37850633068f2be1bc
DiazRock <corolariodiaz@gmail.com>
2022-01-18 10:11:43 -0500

    Added a bootstrap button for test

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -3,9 +3,9 @@
     <img alt="Vue logo" src="@/assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-<!--   <div class="hello">
+  <div class="hello">
     <b-button>Just a Button!</b-button>
-  </div> -->
+  </div>
 </template>
 
 <script>
